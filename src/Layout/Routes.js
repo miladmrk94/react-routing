f844884cd06a6f50022ac42bda9f7f950818b4f7
Milad_Mrk <milad.rastgoou@gmail.com>
2022-05-27 20:59:38 +0430

    [feat] add react-router-dom 6 & customHooks

--- src/Layout/Routes.js
+++ src/Layout/Routes.js
@@ -3,11 +3,6 @@ import HomePage from "../Components/Page/HomePage";
 import AddComment from "./../Components/AddComment";
 import ShowComments from "../Components/ShowComments";
 
-const allPages = [
-  { path: "/Show/:id", component: ShowComments },
-  { path: "/New-Comment", component: AddComment },
-  { path: "/", component: HomePage, exact: true },
-  { path: "*", component: NotFound },
-];
+const allPages = [];
 
 export default allPages;
